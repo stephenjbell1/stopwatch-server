7991acf59945c4abe37f9d2edb25d9f8527e4b79
LAPTOP-6IH21IGU\steph <stephenjbell1990@gmail.com>
2018-02-06 13:08:13 -0600

    fixed user issue

--- common/models/grape.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(Grape) {
-	Grape.beforeRemote('create', function(ctx, modelInstance, next) {
-		ctx.args.data.userId = ctx.req.accessToken.userId;
-		next();
-  });
-};
--- server/server.js
+++ server/server.js
@@ -1,22 +1,15 @@
-'use strict';
-
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var PassportConfigurator =
+  require('loopback-component-passport').PassportConfigurator;
+
 var app = module.exports = loopback();
-//var User = app.models.User;
-//User.hasMany(app.models.Grape, { as: 'Grapes', foreignKey: 'userId' });
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
+    console.log('Web server listening at: %s', app.get('url'));
   });
 };
 
